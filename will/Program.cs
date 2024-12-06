6bd0274b5eb2d0a19cc061d734158f0581041a59
avenaconpalta <willtbtxd@gmail.com>
2024-12-06 15:55:07 -0500

    clase Departamento, venta y propietario

--- will/Program.cs
+++ will/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace will
 {
-    internal class Program
+    public class Departamento
     {
-        static void Main(string[] args)
+        public int NroPartida { get; set; }
+        public string Distrito { get; set; }
+        public double PrecioVenta { get; set; }
+
+        public override string ToString()
         {
+            return $"Partida: {NroPartida}, Distrito: {Distrito}, Precio: {PrecioVenta}";
         }
     }
-}
+
+    public class Propietario
+    {
+        public int Ruc { get; set; }
+        public string RazonSocial { get; set; }
+        public double Ahorros { get; set; }
+
+        public override string ToString()
+        {
+            return $"RUC: {Ruc}, Razón Social: {RazonSocial}, Ahorros: {Ahorros}";
+        }
+    }
+
+    public class Venta
+    {
+        public int Codigo { get; set; }
+        public Departamento Vivienda { get; set; }
+        public Propietario Contratante { get; set; }
+        public double PrecioContrato { get; set; }
+
+        public override string ToString()
+        {
+            return $"Código: {Codigo}, Vivienda: [{Vivienda}], Contratante: [{Contratante}], Precio Contrato: {PrecioContrato}";
+        }
+    }
+
+   
